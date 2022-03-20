76fe735aa16a781eca214a225973d45df40933b1
Joe Banks <joe@jb3.dev>
2022-03-20 22:43:37 +0000

    more fixes

--- spec/jeeves_spec.rb
+++ spec/jeeves_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Jeeves do
   it "has a version number" do
     expect(Jeeves::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
