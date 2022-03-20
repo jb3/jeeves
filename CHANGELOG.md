07ec4277f98d17a648f5dee79f4b415d07c46122
Joe Banks <joe@jb3.dev>
2022-03-20 23:24:30 +0000

    remove all

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## [Unreleased]
-
-## [0.1.0] - 2022-03-20
-
-- Initial release
--- spec/jeeves_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Jeeves do
-  it "has a version number" do
-    expect(Jeeves::VERSION).not_to be nil
-  end
-end
